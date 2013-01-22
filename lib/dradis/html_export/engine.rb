53c8f9d146962692d0894b666ad3357a720d23e6
Daniel Martin <etd-github@nomejortu.com>
2013-01-22 12:06:03 +0100

    Suffle around the Engine code so it makes more sense

--- lib/dradis/html_export/engine.rb
+++ lib/dradis/html_export/engine.rb
@@ -1,14 +1,27 @@
 module Dradis
   module HtmlExport
     class Engine < ::Rails::Engine
-      include Dradis::Core::Plugins::Base
+
+      # Standard Rails Engine stuff
       isolate_namespace Dradis::HtmlExport
       engine_name 'dradis_html_export'
+
+      # use rspec for tests
+      config.generators do |g|
+        g.test_framework :rspec
+      end
+
+      # Connect to the Framework
+      include Dradis::Core::Plugins::Base
 
       plugin_name 'HTML export'
       provides :export
 
       Dradis::Core::Plugins::register(Dradis::HtmlExport)
+
+      initializer "dradis.html_export.init_category" do |app|
+        Dradis::Core::Category.find_or_create_by_name( HtmlExport::Configuration.category )
+      end
     end
   end
 end
