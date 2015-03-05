56495eca43897588d0c6079e6d256c23320ace7d
Jose A. Rilla <jose@rilla.es>
2015-03-05 18:19:13 +0100

    Complete up-to-date implementation of the exporter

--- lib/dradis/plugins/html_export/exporter.rb
+++ lib/dradis/plugins/html_export/exporter.rb
@@ -2,29 +2,62 @@ module Dradis
   module Plugins
     module HtmlExport
 
-      class Exporter # < Dradis::Plugins::Export::Base
-
+      class Exporter < Dradis::Plugins::Export::Base
         # Add auto_link support to the ERB processor (see rails_autolink)
         include ::ActionView::Helpers::TextHelper
+        # For auto_link feature (requires #mail_to)
+        include ::ActionView::Helpers::UrlHelper
 
-        def export(args={})
-          template_content = begin
-            if args[:template]
-              File.read(args[:template])
-            else
-              "<h1>No :template provided</h1>"
+        def export(args = {})
+          community_edition   = Dradis.constants.include?(:Core)
+          model_namespace     = community_edition ? Dradis::Core : nil
+          logger              = args.fetch(:logger, Rails.logger)
+          template_path       = args.fetch(:template)
+          template_properties = ::ReportTemplateProperties.find_by_template_file(File.basename(template_path)) rescue nil
+          project             = args[:project]
+          reporting_cat       = args.fetch(:category, model_namespace::Category.report)
+
+          # Build title
+          title = community_edition ? "Dradis Framework" : "Dradis Professional Edition"
+          title << "- v"
+          title << community_edition ? Dradis::Core::VERSION::STRING : Core::Pro::VERSION::STRING
+          logger.debug{ "Report title: #{title}"}
+
+          # Prepare notes
+          notes = model_namespace::Note.where(category_id: reporting_cat)
+          logger.debug{ "Found #{notes.count} notes assigned to #{reporting_cat.name}."}
+
+          # Prepare issues
+          issuelib = model_namespace::Node.issue_library
+          if issuelib
+            note_ids = issuelib.notes.map(&:id)
+            issues = model_namespace::Issue.find(note_ids)
+
+            # Sort our issues based on the ReportTemplateProperties rules.
+            if template_properties && template_properties.sort_field
+              sort_by = template_properties.sort_field
+
+              logger.debug{ "Template properties define a sort field: #{sort_by}. Sorting..." }
+
+              # FIXME: Assume the Field :type is :number, so cast .to_f and sort
+              issues.sort! do |a, b|
+                b.fields.fetch(sort_by, '0').to_f <=> a.fields.fetch(sort_by, '0').to_f
+              end
+
+              logger.debug{ "Done." }
             end
+
+            # FIXME: This is an ugly piece of code
+            nodes = model_namespace::Evidence.where(issue_id: note_ids).includes(:node).collect(&:node).uniq
+
+            logger.debug{ "Found #{issues.count} issues affecting #{nodes.count} nodes" }
+          else
+            logger.warning { "No issue library node found in this project" }
           end
 
-          # category_name = params.fetch(:category_name, Dradis::Core::Configuration.report)
-          reporting_cat = args.fetch(:category, Dradis::Core::Category.report)
-          reporting_notes_num = Dradis::Core::Note.where(category_id: reporting_cat).count
-          title = "Dradis Framework - v#{Dradis::Core::VERSION::STRING}"
-          notes = Dradis::Core::Note.where(category_id: reporting_cat)
-          issues = Dradis::Core::Issue.find(Dradis::Core::Node.issue_library.notes.pluck(:id))
-
-          erb = ERB.new(template_content)
-          erb.result(binding)
+          # Render template
+          erb = ERB.new( File.read(template_path) )
+          erb.result( binding )
         end
 
         private
@@ -39,7 +72,6 @@ module Dradis
           auto_link(RedCloth.new(output, [:filter_html, :no_span_caps]).to_html, sanitize: false ).html_safe
         end
       end
-
     end
   end
 end
