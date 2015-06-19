3cea139372620fe071758b079db6f126829df59c
George Millo <georgejulianmillo@gmail.com>
2015-06-19 12:28:37 +0200

    bumping version to 3.3.0

--- lib/dradis/plugins/html_export/gem_version.rb
+++ lib/dradis/plugins/html_export/gem_version.rb
@@ -8,9 +8,9 @@ module Dradis
 
       module VERSION
         MAJOR = 3
-        MINOR = 2
-        TINY = 0
-        PRE = nil
+        MINOR = 3
+        TINY  = 0
+        PRE   = nil
 
         STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
       end
