2532260ff6f8db27b850a9579b9414b5b5336bcb
Daniel Martin <etd-github@nomejortu.com>
2016-05-20 11:20:52 +0200

    Bump version number (dependency requirements)

--- lib/dradis/plugins/html_export/gem_version.rb
+++ lib/dradis/plugins/html_export/gem_version.rb
@@ -9,7 +9,7 @@ module Dradis
       module VERSION
         MAJOR = 3
         MINOR = 3
-        TINY  = 2
+        TINY  = 3
         PRE   = nil
 
         STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
