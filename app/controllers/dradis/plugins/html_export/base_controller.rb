065d5e670f52d763749a04c5fe1c9ad89899a787
Daniel Martin <etd-github@nomejortu.com>
2017-02-28 10:25:45 +0100

    Make compativle with dradis-plugins 3.6

--- app/controllers/dradis/plugins/html_export/base_controller.rb
+++ app/controllers/dradis/plugins/html_export/base_controller.rb
@@ -9,12 +9,8 @@ module Dradis
         def index
 
           # these come from Export#create
-          export_manager_hash   = session[:export_manager].with_indifferent_access
-          content_service_class = export_manager_hash[:content_service].constantize
-
-          exporter = Dradis::Plugins::HtmlExport::Exporter.new(
-            content_service: content_service_class.new(plugin: Dradis::Plugins::HtmlExport)
-          )
+          export_manager_hash = session[:export_manager].with_indifferent_access
+          exporter = Dradis::Plugins::HtmlExport::Exporter.new
 
           doc = exporter.export(export_manager_hash)
 
