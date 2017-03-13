8c449c786d5f11a8f73ce52bdccd55632c74f7ee
Daniel Martin <etd-github@nomejortu.com>
2017-03-13 12:10:10 +0100

    Bump version to 3.6

--- lib/dradis/plugins/html_export/engine.rb
+++ lib/dradis/plugins/html_export/engine.rb
@@ -25,18 +25,6 @@ module Dradis
             mount Dradis::Plugins::HtmlExport::Engine => '/export/html'
           end
         end
-
-        # # TODO: could we use this instead?
-        # # https://github.com/spree/spree_analytics/blob/079949fd0e6d9ec87eefd8e3b9c70b5aa3bf25d3/lib/spree_analytics/engine.rb
-        # # Configuration
-        # # mattr_accessor :app_id, :site_id, :token, :api_url, :data_url
-        # # self.template = ''
-        # class Configuration < Dradis::Core::Configurator
-        #   configure :namespace => 'htmlexport'
-        #   setting :category, :default => 'HtmlExport ready'
-        #   # setting :template, :default => Rails.root.join( 'vendor', 'plugins', 'html_export', 'template.html.erb' )
-        #   setting :template, :default => '/Users/etd/dradis/git/dradis-html_export/template.html.erb'
-        # end
       end
     end
   end
