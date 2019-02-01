44e7ebba96d686eb1bdf19d4792cbedb90fa6202
Aaron Manaloto <aaronpomanaloto@gmail.com>
2019-02-01 17:43:03 +0800

    Add gateway_index endpoint for contributor

--- app/controllers/dradis/plugins/html_export/base_controller.rb
+++ app/controllers/dradis/plugins/html_export/base_controller.rb
@@ -2,11 +2,25 @@ module Dradis
   module Plugins
     module HtmlExport
       class BaseController < Dradis::Plugins::Export::BaseController
+        skip_before_action :ensure_tester, only: [:gateway_index]
+
+        def index
+          render_html
+        end
+
+        # This action is similar to the index action but skips the tester check
+        # for the gateway.
+        def gateway_index
+          render_html
+        end
+
+        private
+
         # This method cycles throw the notes in the reporting category and creates
         # a simple HTML report with them.
         #
         # It uses the template at: ./vendor/plugins/html_export/template.html.erb
-        def index
+        def render_html
           exporter = Dradis::Plugins::HtmlExport::Exporter.new(export_options)
           html     = exporter.export
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Dradis::Plugins::HtmlExport::Engine.routes.draw do
   root to: 'base#index'
+
+  get 'gateway_index', to: 'base#gateway_index'
 end
