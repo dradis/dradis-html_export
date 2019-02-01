dc455d0adec46d1c57eba68cc695b03a169c72f8
Aaron Manaloto <aaronpomanaloto@gmail.com>
2019-02-01 18:07:13 +0800

    Use export_options instead

--- app/controllers/dradis/plugins/html_export/base_controller.rb
+++ app/controllers/dradis/plugins/html_export/base_controller.rb
@@ -2,32 +2,25 @@ module Dradis
   module Plugins
     module HtmlExport
       class BaseController < Dradis::Plugins::Export::BaseController
-        skip_before_action :ensure_tester, only: [:gateway_index]
-
-        def index
-          render_html
-        end
-
-        # This action is similar to the index action but skips the tester check
-        # for the gateway.
-        def gateway_index
-          render_html
-        end
-
-        private
+        skip_before_action :ensure_tester, if: -> { gateway? }
 
         # This method cycles throw the notes in the reporting category and creates
         # a simple HTML report with them.
         #
         # It uses the template at: ./vendor/plugins/html_export/template.html.erb
-        def render_html
+        def index
           exporter = Dradis::Plugins::HtmlExport::Exporter.new(export_options)
           html     = exporter.export
 
           render html: html.html_safe
         end
-      end
 
+        private
+
+        def gateway?
+          export_options[:gateway]
+        end
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Dradis::Plugins::HtmlExport::Engine.routes.draw do
   root to: 'base#index'
-
-  get 'gateway_index', to: 'base#gateway_index'
 end
