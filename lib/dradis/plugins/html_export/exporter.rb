1074b07922fe0a7ce97354b356fa9f9f9f158307
Brian Pearce <brian.o.pearce@gmail.com>
2019-07-17 14:41:49 +0200

    Refactor the exporter. Use explicit locals

--- lib/dradis/plugins/html_export/exporter.rb
+++ lib/dradis/plugins/html_export/exporter.rb
@@ -4,57 +4,61 @@ module Dradis
 
       class Exporter < Dradis::Plugins::Export::Base
 
+        def title
+          @title ||= if Dradis.constants.include?(:Pro)
+                       "Dradis Professional Edition v#{Dradis::Pro.version}"
+                     else
+                       "Dradis Community Edition v#{Dradis::CE.version}"
+                     end
+        end
+
+        def sort_issues(issues, template_properties)
+          return unless issues.any? && template_properties&.sort_field
+
+          sort_by = template_properties.sort_field
+
+          logger.debug { "Template properties define a sort field: #{sort_by}. Sorting..." }
+
+          # FIXME: Assume the Field :type is :number, so cast .to_f and sort
+          issues.to_a.sort! do |a, b|
+            b.fields.fetch(sort_by, '0').to_f <=> a.fields.fetch(sort_by, '0').to_f
+          end
+
+          logger.debug { 'Done sorting.' }
+        end
+
         def export(args = {})
           template_path       = options.fetch(:template)
           template_properties = ::ReportTemplateProperties.find_by_template_file(File.basename(template_path)) rescue nil
 
-          user = options[:user]
+          logger.debug { "Report title: #{title}"}
 
-          # Build title
-          title = if Dradis.constants.include?(:Pro)
-                    "Dradis Professional Edition v#{Dradis::Pro.version}"
-                  else
-                    "Dradis Community Edition v#{Dradis::CE.version}"
-                  end
-          logger.debug{ "Report title: #{title}"}
-
-          # Prepare notes
-          reporting_cat = content_service.report_category
-          notes = content_service.all_notes
-          logger.debug{ "Found #{notes.count} notes assigned to the reporting category."}
-
-          # Prepare issues
           issues = content_service.all_issues
-          if issues
-            # Sort our issues based on the ReportTemplateProperties rules.
-            if template_properties && template_properties.sort_field
-              sort_by = template_properties.sort_field
+          sort_issues(issues, template_properties)
 
-              logger.debug{ "Template properties define a sort field: #{sort_by}. Sorting..." }
+          # FIXME: This is an ugly piece of code and the list of nodes should
+          # come from the ContentService.
+          nodes = issues.map(&:evidence).flatten.map(&:node).uniq
 
-              # FIXME: Assume the Field :type is :number, so cast .to_f and sort
-              issues.to_a.sort! do |a, b|
-                b.fields.fetch(sort_by, '0').to_f <=> a.fields.fetch(sort_by, '0').to_f
-              end
+          locals = {
+            issues: issues,
+            nodes: nodes,
+            notes: content_service.all_notes,
+            project: project,
+            reporting_cat: content_service.report_category,
+            title: title,
+            user: options[:user]
+          }
 
-              logger.debug{ "Done." }
-            end
+          logger.debug { "Found #{locals[:notes].count} notes assigned to the reporting category." }
 
-            # FIXME: This is an ugly piece of code and the list of nodes should
-            # come from the ContentService.
-            nodes = issues.map(&:evidence).flatten.map(&:node).uniq
-
+          if issues.any?
             logger.debug{ "Found #{issues.count} issues affecting #{nodes.count} nodes" }
           else
             logger.warning { "No issue library node found in this project" }
           end
 
           # Render template
-          locals = binding.local_variables.map do |var|
-            [var, binding.local_variable_get(var)]
-          end.to_h
-          locals[:project] = project
-
           ApplicationController.render(
             file: template_path,
             layout: false,
