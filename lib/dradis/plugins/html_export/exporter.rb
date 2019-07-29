0342c2095777beffce710f4d5ae969694ebf4f9f
Brian Pearce <brian.o.pearce@gmail.com>
2019-07-29 12:45:10 +0200

    Extract to memoized private methods
    Ensure the work is only done once by memoizing all values. Moved all the debug
    statements to a single place as they made each memoized assignment quite ugly
    and more difficult to read. Now that the values are memoized it's easy enough
    to just call a single reporting method to output all debug values.

--- lib/dradis/plugins/html_export/exporter.rb
+++ lib/dradis/plugins/html_export/exporter.rb
@@ -4,66 +4,77 @@ module Dradis
 
       class Exporter < Dradis::Plugins::Export::Base
 
+        def export(args = {})
+          log_report
+
+          # Render template
+          ApplicationController.render(
+            file: options.fetch(:template),
+            layout: false,
+            locals: {
+              content_service: content_service,
+              issues: issues,
+              nodes: nodes,
+              notes: notes,
+              project: project,
+              reporting_cat: content_service.report_category,
+              title: title,
+              user: options[:user]
+            }
+          )
+        end
+
+        private
+        def log_report
+          logger.debug { "Report title: #{title}" }
+          logger.debug { "Template properties define a sort field: #{sort_field}" }
+
+          if issues&.any?
+            logger.debug { "Found #{issues.count} issues affecting #{nodes.count} nodes" }
+          else
+            logger.warn { 'No issue library node found in this project' }
+          end
+
+          logger.debug { "Found #{notes.count} notes assigned to the reporting category." }
+        end
+
+        def nodes
+          # FIXME: This is an ugly piece of code and the list of nodes should
+          # come from the ContentService.
+          @nodes ||= issues.map(&:evidence).flatten.map(&:node).uniq
+        end
+
+        def notes
+          @notes ||= content_service.all_notes
+        end
+
+        def issues
+          @issues ||= sort_issues content_service.all_issues.includes(:tags)
+        end
+
+        def sort_field
+          @sort_field ||= begin
+            template_path = options.fetch(:template)
+            properties = ::ReportTemplateProperties.find_by_template_file(File.basename(template_path)) rescue nil
+            properties&.sort_field
+          end
+        end
+
+        def sort_issues(unsorted_issues)
+          return unsorted_issues unless unsorted_issues.any? && sort_field
+
+          # FIXME: Assume the Field :type is :number, so cast .to_f and sort
+          unsorted_issues.to_a.sort do |a, b|
+            b.fields.fetch(sort_field, '0').to_f <=> a.fields.fetch(sort_field, '0').to_f
+          end
+        end
+
         def title
           @title ||= if Dradis.constants.include?(:Pro)
                        "Dradis Professional Edition v#{Dradis::Pro.version}"
                      else
                        "Dradis Community Edition v#{Dradis::CE.version}"
                      end
-        end
-
-        def sort_issues(issues, template_properties)
-          return unless issues.any? && template_properties&.sort_field
-
-          sort_by = template_properties.sort_field
-
-          logger.debug { "Template properties define a sort field: #{sort_by}. Sorting..." }
-
-          # FIXME: Assume the Field :type is :number, so cast .to_f and sort
-          issues.to_a.sort! do |a, b|
-            b.fields.fetch(sort_by, '0').to_f <=> a.fields.fetch(sort_by, '0').to_f
-          end
-
-          logger.debug { 'Done sorting.' }
-        end
-
-        def export(args = {})
-          template_path       = options.fetch(:template)
-          template_properties = ::ReportTemplateProperties.find_by_template_file(File.basename(template_path)) rescue nil
-
-          logger.debug { "Report title: #{title}"}
-
-          issues = content_service.all_issues
-          sort_issues(issues, template_properties)
-
-          # FIXME: This is an ugly piece of code and the list of nodes should
-          # come from the ContentService.
-          nodes = issues.map(&:evidence).flatten.map(&:node).uniq
-
-          locals = {
-            issues: issues,
-            nodes: nodes,
-            notes: content_service.all_notes,
-            project: project,
-            reporting_cat: content_service.report_category,
-            title: title,
-            user: options[:user]
-          }
-
-          logger.debug { "Found #{locals[:notes].count} notes assigned to the reporting category." }
-
-          if issues.any?
-            logger.debug{ "Found #{issues.count} issues affecting #{nodes.count} nodes" }
-          else
-            logger.warning { "No issue library node found in this project" }
-          end
-
-          # Render template
-          ApplicationController.render(
-            file: template_path,
-            layout: false,
-            locals: locals
-          )
         end
       end
     end
