1d825b62a968ca394b1525aa27bd9cce9d390083
Xavi Vila <xavivm@gmail.com>
2019-08-28 14:49:50 +0100

    initialize html exporter with request params instead of pararms stored in session

--- app/controllers/dradis/plugins/html_export/base_controller.rb
+++ app/controllers/dradis/plugins/html_export/base_controller.rb
@@ -2,18 +2,14 @@ module Dradis
   module Plugins
     module HtmlExport
       class BaseController < Dradis::Plugins::Export::BaseController
-        # This method cycles throw the notes in the reporting category and creates
-        # a simple HTML report with them.
-        #
-        # It uses the template at: ./vendor/plugins/html_export/template.html.erb
+
         def index
-          exporter = Dradis::Plugins::HtmlExport::Exporter.new(export_options)
+          exporter = Dradis::Plugins::HtmlExport::Exporter.new(params)
           html     = exporter.export
 
           render html: html.html_safe
         end
       end
-
     end
   end
 end
