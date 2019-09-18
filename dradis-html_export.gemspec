c68ec9906d2b1731175d8147d9f279c219e739bb
Xavi Vila <xavivm@gmail.com>
2019-09-18 18:25:00 +0100

    get rspec and capybara ready

--- dradis-html_export.gemspec
+++ dradis-html_export.gemspec
@@ -20,16 +20,16 @@ Gem::Specification.new do |spec|
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
 
-  # gem.add_dependency 'dradis_core', version
   spec.add_dependency 'dradis-plugins', '~> 3.6'
 
   # Note markup
   spec.add_dependency 'rails_autolink', '~> 1.1'
   spec.add_dependency 'RedCloth', '~> 4.3.2'
 
-  # gem.add_development_dependency 'capybara', '~> 1.1.3'
+  spec.add_development_dependency 'byebug'
+  spec.add_development_dependency 'capybara'
   # gem.add_development_dependency 'database_cleaner'
-  # gem.add_development_dependency 'factory_girl_rails'
-  # gem.add_development_dependency 'rspec-rails',  '~> 2.11.0'
-  # gem.add_development_dependency 'sqlite3'
+  # gem.add_development_dependency 'factory_bot_rails'
+  spec.add_development_dependency 'rspec-rails'
+  spec.add_development_dependency 'sqlite3'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,39 +1,43 @@
 # Configure Rails Envinronment
 ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../dummy/config/environment", __FILE__)
-
+require_relative "../spec/dummy/config/environment"
+# ActiveRecord::Migrator.migrations_paths = [File.expand_path("../spec/dummy/db/migrate", __dir__)]
 require 'rspec/rails'
+require 'capybara/rails'
+require 'capybara/rspec'
+require 'byebug'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
-require 'database_cleaner'
-require 'factory_girl_rails'
+# require 'database_cleaner'
+# require 'factory_bot_rails'
 
 
 RSpec.configure do |config|
+  config.include Capybara::DSL
 
   # Capybara javascript drivers require transactional fixtures set to false,
   # and we just use DatabaseCleaner to cleanup after each test instead.
   # Without transactional fixtures set to false none of the records created to
   # setup a test will be available to the browser, which runs under a seperate
   # server instance.
-  config.use_transactional_fixtures = false
-
-  config.before(:each) do
-    if example.metadata[:js]
-      DatabaseCleaner.strategy = :truncation
-    else
-      DatabaseCleaner.strategy = :transaction
-    end
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
+  # config.use_transactional_fixtures = false
+  #
+  # config.before(:each) do
+  #   if example.metadata[:js]
+  #     DatabaseCleaner.strategy = :truncation
+  #   else
+  #     DatabaseCleaner.strategy = :transaction
+  #   end
+  # end
+  #
+  # config.before(:each) do
+  #   DatabaseCleaner.start
+  # end
+  #
+  # config.after(:each) do
+  #   DatabaseCleaner.clean
+  # end
 end
