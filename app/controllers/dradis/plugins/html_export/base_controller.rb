a6a48e0d6495839adae09ff1a20e1ce9e9fe7073
Xavi Vila <xavivm@gmail.com>
2019-09-18 17:55:43 +0100

    using strong pararms

--- app/controllers/dradis/plugins/html_export/base_controller.rb
+++ app/controllers/dradis/plugins/html_export/base_controller.rb
@@ -4,10 +4,16 @@ module Dradis
       class BaseController < Dradis::Plugins::Export::BaseController
 
         def index
-          exporter = Dradis::Plugins::HtmlExport::Exporter.new(params)
+          exporter = Dradis::Plugins::HtmlExport::Exporter.new(export_params)
           html     = exporter.export
 
           render html: html.html_safe
+        end
+
+        private
+
+        def export_params
+          params.permit(:project_id, :template)
         end
       end
     end
