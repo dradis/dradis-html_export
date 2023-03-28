d00f2c00bc1f0778c3fe7d20899980703ae6aaa3
Aaron Manaloto <mail@aapomm.io>
2023-03-28 20:05:52 +0800

    Remove validation

--- app/controllers/dradis/plugins/html_export/base_controller.rb
+++ app/controllers/dradis/plugins/html_export/base_controller.rb
@@ -2,8 +2,6 @@ module Dradis
   module Plugins
     module HtmlExport
       class BaseController < Dradis::Plugins::Export::BaseController
-        before_action :validate_scope
-
         # This method cycles throw the notes in the reporting category and creates
         # a simple HTML report with them.
         #
@@ -15,16 +13,6 @@ module Dradis
           html = exporter.export
 
           render html: html.html_safe
-        end
-
-        private
-
-        def validate_scope
-          @scope = params[:scope]
-
-          unless @scope == 'all' || @scope == 'published'
-            raise 'Something fishy is going on...'
-          end
         end
       end
     end
