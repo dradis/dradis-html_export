c688fddeff3a4be57c911add48ce1765beb28e98
Aaron Manaloto <mail@aapomm.io>
2024-05-07 14:25:43 +0800

    Use version 4.12.0

--- lib/dradis/plugins/html_export/gem_version.rb
+++ lib/dradis/plugins/html_export/gem_version.rb
@@ -9,7 +9,7 @@ module Dradis
       module VERSION
         MAJOR = 4
         MINOR = 12
-        TINY  = 1
+        TINY  = 0
         PRE   = nil
 
         STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
